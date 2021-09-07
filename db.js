34bb6f9a7c0f6048fddf9b71c2ce0e067ada20e1
Helena Bliss <helenajbliss75@gmail.com>
2021-09-07 11:54:54 -0500

    Completed Hash Section

--- db.js
+++ db.js
@@ -1,82 +1,88 @@
-const jwt = require('jsonwebtoken')
-const Sequelize = require('sequelize');
+const jwt = require("jsonwebtoken");
+const Sequelize = require("sequelize");
 const { STRING } = Sequelize;
 const config = {
-  logging: false
+  logging: false,
 };
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-const tokenSecret = process.env.JWT
+const tokenSecret = process.env.JWT;
 
-if(process.env.LOGGING){
+if (process.env.LOGGING) {
   delete config.logging;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db', config);
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/acme_db",
+  config
+);
 
-const User = conn.define('user', {
+const User = conn.define("user", {
   username: STRING,
-  password: STRING
+  password: STRING,
 });
 
-User.byToken = async(token)=> {
-  console.log("---user by token")
+User.byToken = async (token) => {
   try {
-    const verifiedToken = await jwt.verify(token, process.env.JWT)
-    console.log('verified token', verifiedToken)
+    const verifiedToken = await jwt.verify(token, process.env.JWT);
+
     const user = await User.findByPk(verifiedToken.userId);
-    if(user){
+    if (user) {
       return user;
     }
-    const error = Error('bad credentials');
+    const error = Error("bad credentials");
     error.status = 401;
     throw error;
-  }
-  catch(ex){
-    const error = Error('bad credentials');
+  } catch (ex) {
+    const error = Error("bad credentials");
     error.status = 401;
     throw error;
   }
 };
 
-User.authenticate = async({ username, password })=> {
-  console.log("---USER.AUTHENTICATE")
+User.authenticate = async ({ username, password }) => {
   const user = await User.findOne({
     where: {
       username,
-      password
-    }
+    },
   });
-  if(user){
-    const token = await jwt.sign( {userId: user.id}, tokenSecret)
-    console.log('token', token)
+  const correct = await bcrypt.compare(password, user.password);
+  if (correct) {
+    const token = await jwt.sign({ userId: user.id }, tokenSecret);
+
     return token;
   }
-  const error = Error('bad credentials');
+  const error = Error("bad credentials");
   error.status = 401;
   throw error;
 };
 
-const syncAndSeed = async()=> {
+const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const credentials = [
-    { username: 'lucy', password: 'lucy_pw'},
-    { username: 'moe', password: 'moe_pw'},
-    { username: 'larry', password: 'larry_pw'}
+    { username: "lucy", password: "lucy_pw" },
+    { username: "moe", password: "moe_pw" },
+    { username: "larry", password: "larry_pw" },
   ];
   const [lucy, moe, larry] = await Promise.all(
-    credentials.map( credential => User.create(credential))
+    credentials.map((credential) => User.create(credential))
   );
   return {
     users: {
       lucy,
       moe,
-      larry
-    }
+      larry,
+    },
   };
 };
+
+User.beforeCreate(async (user) => {
+  user.password = await bcrypt.hash(user.password, saltRounds);
+});
 
 module.exports = {
   syncAndSeed,
   models: {
-    User
-  }
+    User,
+  },
 };
